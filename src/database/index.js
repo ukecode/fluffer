c9020e99be77df151e690a982e7b2a3ceae19432
kaiofprates <kaioprates@hotmail.com>
2020-04-09 19:15:24 -0300

    doing the good things

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const result = require('dotenv').config({ path: __dirname + '/../.env' });
 
-const dataBaseUrl = "mongodb+srv://ukecode:carbon14@cluster0-ippbo.mongodb.net/fluffer?retryWrites=true&w=majority"
+const dataBaseUrl = process.env.DB_ATLAS;
 
 mongoose.connect(dataBaseUrl, {
     useNewUrlParser: true,
